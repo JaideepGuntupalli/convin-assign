10cb90e5437ea7ca3853d378cce55b7945c37089
Jaideep <guntupallijaideep@gmail.com>
2023-01-05 23:57:01 +0530

    misc: cleanup

--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,7 @@ import { Provider } from "react-redux";
 import store from "./app/store";
 
 const container = document.getElementById("root");
-const root = createRoot(container); // createRoot(container!) if you use TypeScript
+const root = createRoot(container);
 root.render(
     <Provider store={store}>
         <App />
